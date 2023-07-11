61084d501972cd95e89530d6503531c4ecfc0ff5
Ttoorne <karakozhaev.bektur@gmail.com>
2023-07-11 18:57:45 +0600

    notfound

--- src/components/notfound/NotFound.jsx
+++ src/components/notfound/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./notFound.css";
+import { Button } from "react-bootstrap";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="page_not_found">
+      <h1>Страница Не Найдена</h1>
+      <Button>Вернуться на главную страницу</Button>
+      <img src="" alt="" />
+    </div>
+  );
 };
 
 export default NotFound;
