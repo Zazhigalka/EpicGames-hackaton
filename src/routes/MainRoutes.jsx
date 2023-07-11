fdb0d868179a1f2fc98593d34fa94d4a01ab0e6a
Ttoorne <karakozhaev.bektur@gmail.com>
2023-07-11 21:35:16 +0600

    first

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AuthPage from "../pages/AuthPage";
-import NotFound from "../components/notfound/NotFound";
-import Cart from "../components/cart/Cart";
-import WishList from "../components/wishList/WishList";
 import WishListPage from "../pages/WishListPage";
 import CartPage from "../pages/CartPage";
 import NotFoundPage from "../pages/NotFoundPage";
+import ProductDetailsPage from "../pages/ProductDetailsPage";
 
 const MainRoutes = () => {
   return (
