2fc989b691d9d2d8eddf955a24f6ff02b89cf84e
Ttoorne <karakozhaev.bektur@gmail.com>
2023-07-14 20:58:38 +0600

    home-page start

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Search from "../search/Search";
+import "./home.css";
+import HomeCarousel from "./HomeCarousel";
 
 const Home = () => {
   return (
-    <div>
-      <div style={{ position: "sticky", top: "0px", left: "0", right: "0" }}>
+    <div className="home__page">
+      <div
+        style={{
+          position: "sticky",
+          top: "0px",
+          left: "0",
+          right: "0",
+          zIndex: 10,
+        }}
+      >
         <Search />
       </div>
+      <HomeCarousel />
     </div>
   );
 };
