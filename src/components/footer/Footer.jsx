3364af3ee59b68e894ceefc46a042052c09211d0
dudiiiisss <toktorbaevaaikol@gmail.com>
2023-07-17 19:49:53 +0600

    wishList adaptive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import "./Footer.css";
+import "./footer.css";
 import "./Footer-adaptive-styles.css";
 import { ReactComponent as YoutubeIcon } from "../../assets/youtube.svg";
 import { ReactComponent as TwitterIcon } from "../../assets/twitter.svg";
